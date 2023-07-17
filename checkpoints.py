fab26885ff3fdeaccd838787667dda350e7d28b5
Jeffrey Wang <66625372+JeffreyWangDev@users.noreply.github.com>
2023-07-17 09:09:01 -0400

    asd

--- checkpoints.py
+++ checkpoints.py
@@ -27,7 +27,17 @@ Input: [0, 3, 4, 9] ; Output: 5
 Input: [10, 8, 4, 1] ; Output: 4
 Input: [5, 0, 3, 6] ; Output: 3
 """
-
+def selectionSort(array, size):
+    
+    for ind in range(size):
+        min_index = ind
+ 
+        for j in range(ind + 1, size):
+            # select the minimum element in every iteration
+            if array[j] < array[min_index]:
+                min_index = j
+         # swapping the elements to sort the array
+        (array[ind], array[min_index]) = (array[min_index], array[ind])
 class Solution:    
     def longestdistance(self, checkpoints):
             # Sort checkpoints from least to greatest
@@ -35,6 +45,7 @@ class Solution:
             #return type: int
             
             #TODO: Write code below to returnn an int with the solution to the prompt.
+            selectionSort(checkpoints,len(checkpoints) )
             max = 0
             index = 1
             for i in checkpoints[:-1]:
